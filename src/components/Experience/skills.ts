e27b180033f321d32921fd7384836895fe6bce53
Certsoftt <ukpeubong8573@gmail.com>
2025-04-15 20:17:43 +0100

    Create skill.ts object structure for industrial section

---
+++ src/components/Experience/skills.ts
@@ -0,0 +1,383 @@
+import {CodeOutlined, Merge, FormatPaint, Cloud, AppBlocking, Storage, GitHub, Analytics, NoteRounded, Build} from "@mui/icons-material";
+import { Skill } from "./type";
+
+export const skills: Skill = [
+      {
+        "title": "• Automation/Testing Tool",
+        "content": "N8N, TypeForm, Git Actions(CI/CD), BrowserStack",
+        "icon": NoteRounded,
+        "sectionOne":[
+          {
+            "id":"Automation/Testing Tool",
+            "Experience": "Personal/Internship",
+            "Organization": [
+              {
+                "3MTT":"https://app.3mtt.training/"
+              }
+            ],
+            "Position":"Software Developer",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id": "Automation_projects",
+            "Project": [
+              {
+                "Faztar":"",
+              },
+              {
+                "Portfolio Site":"",
+              },
+              {
+                "CodePack Editor": ""
+              }
+            ],
+            "Goal": "Built software as a service (SaaS) with automated workflow for code delivery, testing, release, preview and production with Git and Vercel, and provides solution with cleaning, fixing, and improving code quality ",
+            "JobType": "Freelance"
+          }
+        ]
+      },
+      {
+        "title": "• Design Library/Framework",
+        "content": "Styled Components, React MaterialUI, Ant Design ",
+        "icon": FormatPaint,
+        "sectionOne": [
+          {
+            "id":"Design Library/Framework",
+            "Experience": "Industrial",
+            "Organization": [
+              {
+                "Gterahub Limited":"https://gterahub.com"
+              }
+            ],
+            "Position":"Web Developer",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id": "Design_Libraries_project",
+            "Project": [
+              {
+                "Witgirl": "https://witgirl.org"
+              }
+            ],
+            "Goal": "Improve web app responsiveness and performance, reduce the use of lot dependencies by refactoring the design using React MaterialUI and redesigning re-usable components ",
+            "JobType": "Contract (6 months)"
+          }
+        ]
+      },
+      {
+        "title": "• Cloud Technologies",
+        "content": "Firebase, ArgoCD, NocoDB, Azure",
+        "icon": Cloud,
+        "sectionOne":[
+          {
+            "id":"Cloud Technologies",
+            "Experience": "Personal",
+            "Organization": [
+              {
+                "None": "#"
+              }
+            ],
+            "Position": "DevOps",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id": "Cloud Technologies_project",
+            "Project": [
+              {
+                "Auth Web App":"https://makemore.netlify.app/"
+              }
+            ],
+            "Goal": "Auth system that synchronizes data from Google firebase and Google firestore store for: • OTP login, • SignIn with Google, • Email Verification & Update Password, • Cookies/Sessions and Logout",
+            "JobType": "Freelance"
+          }
+        ]
+      },
+      {
+        "title": "• Programming Languages",
+        "content": "C++, PHP, Solidity, JavaScript, TypeScript, Python",
+        "icon": CodeOutlined,
+        "sectionOne": [
+          {
+            "id":"Programming Languages",
+            "Experience": "Industrial",
+            "Organization": [
+              {
+                "Gterahub Limited" : "https://gterahub.com"
+              },
+              {
+                "Techminds Academy" : "https://techmindsacademy.org"
+              }
+            ],
+            "Position": "Full Stack Developer",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id": "Programming Languages_project",
+            "Project": [
+              {
+                "Gterahub": "https://gterahub.com",
+              },
+              {
+                "Tech Minds Academy": "https://techmindsacademy.org"
+              }
+            ],
+            "Goal": "Contributed to the Gterahub business web app design and the microservices. Designed TechmindsAcademy LMS that works with B2B and B2C model",
+            "JobType": "Contract (3-6 months)"
+          }
+        ]
+      },
+      {
+        "title": "• Web & Mobile Development",
+        "content": "Truffle, React, NextJS, ExpressJS, Restful API, React Native, Laravel",
+        "icon": AppBlocking,
+        "sectionOne":[
+          {
+            "id":"Web & Mobile Development",
+            "Experience": "Industrial/Personal/Internship",
+            "Organization":[
+              {
+                "Gterahub Limited": "https://gterahub.com",
+              },
+              {
+                "Techminds Academy": "https://techmindsacademy.org",
+              },
+              {
+                "Wedigraf Technologies": "https://wedigraf.com"
+              }
+            ],
+            "Position": "Software Developer & Instructor",
+          }
+        ],
+        "sectionTwo": [
+          {
+            "id": "Web & Mobile Development_project",
+            "Project": [
+              {
+                "Service Delivery app": "<repo_link>",
+              },
+              {
+                "Vendor Finder Platform": "https://vendorpoint.netlify.app"
+              },
+              {
+                "Web3 Wallet": "https://github.com/certsoftt/web3-wallet",
+              },
+              {
+                "Dynamic Form": "https://reactmuiform.netlify.app/",
+              },
+              {
+                "Job Listing App": "https://github.com/Certsoftt/job-app",
+              }
+            ],
+            "Goal": "Designed a lightweight NextJS page that supports server-side rendering (SSR) and handles performance issues gracefully. Design service delivery cross-platform application with core functionalities that works both offline and online.  Good user experience and performance.",
+            "JobType": "Freelance & Contract"
+          }
+        ]
+      },
+      {
+        "title": "• Database Management",
+        "content": "Firebase, FireStore, PostgreSQL, MySQL, NocoDB",
+        "icon": Storage,
+        "sectionOne": [
+          {
+            "id": "Database Management",
+            "Experience": "Industrial/Personal/Internship",
+            "Organization":[
+              {
+                "Gterahub Limited": "https://gterahub.com",
+              },
+              {
+                "Techminds Academy": "https://techmindsacademy.org",
+              },
+              {
+                
+                "Wedigraf Technologies": "https://wedigraf.com"
+              }
+            ],
+            "Position": "Software Developer/ Instructor",
+          }
+        ],
+        "sectionTwo": [
+          {
+            "id": "Database Management_project",
+            "Project":[
+              {
+                "Auth Web App":"https://makemore.netlify.app/",
+              },
+              {
+                "Web3 Wallet": "https://github.com/certsoftt/web3-wallet",
+              },
+              {
+                "Vendor Finder Platform": "https://vendorpoint.netlify.app"
+              }
+            ],
+            "Goal": "",
+            "JobType": "Part-Time & Contract (3-6 months)"
+          }
+        ]
+      },
+      {
+        "title": "• Microservices & APIs",
+        "content": "Web API",
+        "icon": CodeOutlined,
+        "sectionOne":[
+          {
+            "id":"Microservices & APIs",
+            "Experience": "Industrial",
+            "Organization": [
+              {
+                "NIGGS Global | Alora": "#"
+              }
+            ],
+            "Position": "Software Developer",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id":"Microservices & APIs_project",
+            "Project": [
+              {
+                "Alora": "#"
+              }
+            ],
+            "Goal": "A multipurpose ecommerce solution that implements B2B and B2C model, with microservices such as navigator app, vendor finder app",
+            "JobType": "Remote"
+          }
+        ]
+      },
+      {
+        "title": "• Software Architecture",
+        "content": "SOLID Principle, MVC, design patterns, scalable solutions",
+        "icon": Build,
+        "sectionOne":[
+          {
+            "id":"Software Architecture",
+            "Experience": "Industrial",
+            "Organization": [
+              {
+                "Gterahub Limited":"https://gterahub.com",
+              },
+              {
+                "Wedigraf Technologies": "https://wedigraf.com"
+              },
+              {
+                "Techminds Academy": "https://techmindsacademy.org",
+              }
+            ],
+            "Position": "Software Developer/ Instructor",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id": "Software Architecture_project",
+            "Project": [
+              {
+                "Web Apps": "#"
+              }
+            ],
+          "Goal": "",
+          "JobType": "Contract (3-6 months)"
+          }
+        ]
+      },
+      {
+        "title": "• Version Control",
+        "content": "Git, NPM",
+        "icon": GitHub,
+        "sectionOne":[
+          {
+            "id":"Version Control",
+            "Experience": "Industrial",
+            "Organization": [
+              {
+                "3MTT": "https://app.3mtt.training/"
+              }
+            ],
+            "Position": "Software Developer/ Instructor",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id":"Version Control_project",
+            "Project":[
+              {
+                "Live Chat Widget": "https://www.npmjs.com/package/ubsimple-chat-widget",
+              },
+              {
+                "Common Things API": "https://www.npmjs.com/package/common-things-api", 
+              },
+              {
+                "Faztar": "https://www.npmjs.com/package/cra-template-faztar",
+              },
+              {
+                "Basic Auth": "https://www.npmjs.com/package/ubsimple-basic-auth"
+              }
+            ],
+            "Goal": "Built packages and templates that provides regular common services but in more flexible manner with goal of making usability and implementation of such serivces more easier.",
+            "JobType": "Freelance"
+          }
+        ]
+      },
+      {
+        "title": "• Static Code Analysis Tool",
+        "content": "ESLint, Prettier, ESLint-Stylistic",
+        "icon": Merge,
+        "sectionOne":[
+          {
+            "id":"Static Code Analysis Tool",
+            "Experience": "Personal/Internship",
+            "Organization": [
+              {
+                "None": "#"
+              }
+            ],
+            "Position": "Software Developer",
+          }
+        ],
+        "sectionTwo": [
+          {
+            "id": "Static Code Analysis Tool_project",
+            "Project":[
+              {
+                "Faztar": "https://www.npmjs.com/package/cra-template-faztar",
+                "CodePack Editor": "#"
+              }
+            ],
+            "Goal": "",
+            "JobType": "Freelance"
+          }
+        ]
+      },
+      {
+        "title": "• Software Testing Frameworks/Libraries",
+        "content": "Jest, Cypress, Pest, RTL",
+        "icon": NoteRounded,
+        "sectionOne":[
+          {
+            "id":"Software Testing Frameworks/Libraries",
+            "Experience": "Personal/Internship",
+            "Organization": [
+              {
+                "None": "#"
+              }
+            ],
+            "Position": "Software Tester",
+          }
+        ],
+        "sectionTwo":[
+          {
+            "id": "Software Testing Frameworks/Libraries_project",
+            "Project": [
+              {
+                "None": "#"
+              }
+            ],
+            "Goal": "",
+            "JobType": "Freelance"
+          }
+        ]
+      }
+]
