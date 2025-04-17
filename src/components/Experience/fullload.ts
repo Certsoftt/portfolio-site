ae6d67a2940a112a7f4e3ba08121241fbe1f3bf9
Certsoftt <ukpeubong8573@gmail.com>
2025-04-18 00:32:22 +0100

    Create full_load.ts to enhance lazy loading

---
+++ src/components/Experience/fullload.ts
@@ -0,0 +1,203 @@
+import {CodeOutlined, Merge, FormatPaint, Cloud, AppBlocking, Storage, GitHub, Analytics, NoteRounded, Build} from "@mui/icons-material";
+import { Skill } from "./type";
+
+export const fullload: Skill = [
+      {
+        "title": "• Automation/Testing Tool",
+        "content": "N8N, TypeForm, Git Actions(CI/CD), BrowserStack",
+        "icon": NoteRounded,
+        "section":[
+          {
+            "id":"Automation/Testing Tool",
+            "Project": [
+                ["Faztar","https://"],
+                ["CodePack Editor", ""],
+                ["Portfolio Site",""],
+              ],
+           "JobType": "Freelance",
+            "Goal":"Built software as a service (SaaS) with automated workflow for code delivery, testing, release, preview and production with Git and Vercel, and provides solution with cleaning, fixing, and improving code quality ",
+          }
+        ],
+      },
+      {
+        "title": "• Design Library/Framework",
+        "content": "Styled Components, React MaterialUI, Ant Design ",
+        "icon": FormatPaint,
+        "section": [
+          {
+            "id":"Design Library/Framework",
+            "Project": [
+              [
+                "Witgirl", "https://witgirl.org"
+              ]
+            ],
+            "Goal": "Improve web app responsiveness and performance, reduce the use of lot dependencies by refactoring the design using React MaterialUI and redesigning re-usable components ",
+            "JobType": "Contract (6 months)"
+          }
+        ]
+      },
+      {
+        "title": "• Cloud Technologies",
+        "content": "Firebase, ArgoCD, NocoDB, Azure",
+        "icon": Cloud,
+        "section":[
+          {
+            "id":"Cloud Technologies",
+            "Project": [
+              [
+                "Auth Web App","https://makemore.netlify.app/"
+              ]
+            ],
+            "Goal": "Auth system that synchronizes data from Google firebase and Google firestore store for: • OTP login, • SignIn with Google, • Email Verification & Update Password, • Cookies/Sessions and Logout",
+            "JobType": "Freelance"
+          }
+        ]
+      },
+      {
+        "title": "• Programming Languages",
+        "content": "C++, PHP, Solidity, JavaScript, TypeScript, Python",
+        "icon": CodeOutlined,
+        "section": [
+          {
+            "id":"Programming Languages",
+            "Project": [
+            
+                ["Gterahub Limited", "https://gterahub.com"],
+                ["Techminds Academy", "https://techmindsacademy.org"]
+              
+            ],
+            "Goal": "Contributed to the Gterahub business web app design and the microservices. Designed TechmindsAcademy LMS that works with B2B and B2C model",
+            "JobType": "Contract (3-6 months)"
+          }
+        ],
+      },
+      {
+        "title": "• Web & Mobile Development",
+        "content": "Truffle, React, NextJS, ExpressJS, Restful API, React Native, Laravel",
+        "icon": AppBlocking,
+        "section":[
+          {
+            "id":"Web & Mobile Development",
+            "Project": [
+              
+                ["SD App", "<repo_link>"],
+                ["JL App", "https://github.com/Certsoftt/job-app"],
+                ["Dynamic Form", "https://reactmuiform.netlify.app/"],
+                ["Web3 Wallet", "<repo_link>"],
+                ["VFP", "https://vendorpoint.netlify.app"]
+              
+            ],
+            "Goal": "Designed a lightweight NextJS page that supports server-side rendering (SSR) and handles performance issues gracefully. Design service delivery cross-platform application with core functionalities that works both offline and online.  Good user experience and performance.",
+            "JobType": "Freelance & Contract"
+          }
+        ],
+      },
+      {
+        "title": "• Database Management",
+        "content": "Firebase, FireStore, PostgreSQL, MySQL, NocoDB",
+        "icon": Storage,
+        "section": [
+          {
+            "id": "Database Management",
+            "Project":[
+              
+                ["Auth Web App","https://makemore.netlify.app/"],
+                ["Web3 Wallet", "<repo_link>"],
+                ["Vendor Finder Platform", "https://vendorpoint.netlify.app"]
+              
+            ],
+            "Goal": "",
+            "JobType": "Part-Time & Contract (3-6 months)"
+          }
+        ],
+      },
+      {
+        "title": "• Microservices & APIs",
+        "content": "Web API",
+        "icon": CodeOutlined,
+        "section":[
+          {
+            "id":"Microservices & APIs",
+            "Project": [
+            
+                ["Alora", "#"]
+              
+            ],
+            "Goal": "A multipurpose ecommerce solution that implements B2B and B2C model, with microservices such as navigator app, vendor finder app",
+            "JobType": "Remote"
+          }
+        ],
+      },
+      {
+        "title": "• Software Architecture",
+        "content": "SOLID Principle, MVC, design patterns, scalable solutions",
+        "icon": Build,
+        "section":[
+          {
+            "id":"Software Architecture",
+            "Project": [
+              
+                ["Web Apps", "#"]
+              
+            ],
+          "Goal": "",
+          "JobType": "Contract (3-6 months)"
+          }
+        ],
+      },
+      {
+        "title": "• Version Control",
+        "content": "Git, NPM",
+        "icon": GitHub,
+        "section":[
+          {
+            "id":"Version Control",
+            "Project":[
+              
+                ["LCW", "https://www.npmjs.com/package/ubsimple-chat-widget"], 
+                ["CT API", "#"], 
+                ["Faztar", "https://www.npmjs.com/package/cra-template-faztar"], 
+                ["Basic Auth", "https://www.npmjs.com/package/ubsimple-basic-auth"]
+              
+            ],
+            "Goal": "Built packages and templates that provides regular common services but in more flexible manner with goal of making usability and implementation of such serivces more easier.",
+            "JobType": "Freelance"
+          }
+        ],
+      },
+      {
+        "title": "• Static Code Analysis Tool",
+        "content": "ESLint, Prettier, ESLint-Stylistic",
+        "icon": Merge,
+        "section":[
+          {
+            "id":"Static Code Analysis Tool",
+            "Project":[
+              
+                ["Faztar", "https://www.npmjs.com/package/cra-template-faztar"],
+                ["CodePack Editor", "#"]
+              
+            ],
+            "Goal": "",
+            "JobType": "Freelance"
+          }
+        ],
+      },
+      {
+        "title": "• Software Testing Frameworks/Libraries",
+        "content": "Jest, Cypress, Pest, RTL",
+        "icon": Analytics,
+        "section":[
+          {
+            "id":"Software Testing Frameworks/Libraries",
+            "Project": [
+              
+                ["None", "#"]
+              
+            ],
+            "Goal": "",
+            "JobType": "Freelance"
+          }
+        ],
+      }
+]
