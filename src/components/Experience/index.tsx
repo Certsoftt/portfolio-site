b99aab2bea35e17841993c845462e878afc7b19d
Certsoftt <ukpeubong8573@gmail.com>
2025-04-18 00:33:57 +0100

    Add: load more button for lazy loading

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Container from "../../common/Container"
 import {Row, Col} from "antd"
-import {Stack, IconButton, Typography, Chip} from "@mui/material";
+import {Stack, IconButton, Typography, Chip, Button} from "@mui/material";
+// import {LoadingButton} from '@mui/lab'
 import { Slide } from "react-awesome-reveal";
 import {withTranslation, TFunction } from 'react-i18next';
 import { DivContainer, PaperContainer } from '../Repo/styles'
 import {skills} from './skills' 
+import {fullload} from './fullload' 
+import { Skill } from './type';
 
 const Experience = ({ t }: { t: TFunction }) => {
+  const [skill, setSkill] = useState<Skill | null>(null)
+  const [clicked, setClicked] = useState(false)
+  useEffect(()=>{
+    setSkill(skills)
+  },[])
+  const loadMore = ()=>{
+    setSkill(fullload)
+    setClicked(true)
+  }
   return (
     <React.Fragment>
       <Container>
@@ -17,68 +29,36 @@ const Experience = ({ t }: { t: TFunction }) => {
           <Slide direction="up" triggerOnce>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
               {
-                skills.map(skill=>
-                  <Col xs={24} sm={24} md={24} lg={24} xl={24} key={skill.title}>
+                skill?.map(displayskill=>
+                  <Col xs={24} sm={24} md={12} lg={12} xl={12} key={displayskill.title}>
                     <DivContainer>
                       <PaperContainer elevation={4}>
                         <Stack direction="row" justifyContent="normal" spacing={1} alignItems="center" sx={{marginBottom:"10px"}}>
-                          <IconButton size="small" sx={{color:"blueviolet"}}>
-                            <skill.icon/>
+                          <IconButton size="small" sx={{color:"black"}}>
+                            <displayskill.icon/>
                           </IconButton>
-                          <Typography variant="h6" component="h6">{t(`${skill.title}`)}</Typography>
+                          <Typography component="h6">{t(`${displayskill.title}`)}</Typography>
+                          <Chip label={`${displayskill.section[0].JobType}`} variant="outlined" sx={{color:"white", backgroundColor:"rgb(255, 130, 92)"}}/>
                         </Stack>
-                        <Stack direction="row" sx={{marginBottom:"10px"}}>
-                        <Typography component="p"> {t(`${skill.content}`)}</Typography>
+                        <Stack direction="row" justifyContent="normal" spacing={1} alignItems="center" sx={{marginBottom:"10px"}}>
+                        <Chip label={`Skill`} variant="outlined" sx={{color:"white", backgroundColor:"#2e186a"}}/>
+                        <Typography component="p"> {t(`${displayskill.content}`)}</Typography>
                         </Stack>
-                        <Stack direction="row" justifyContent="space-between" alignItems="center">
-                          <Stack direction="column" spacing={2}>
-                            {skill.sectionOne.map(sectionone=>
-                              <React.Fragment key={sectionone.id}>
+                        {/* <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}> */}
+                          {/* <Col xs={24} sm={24} md={12} lg={12} xl={12}> */}
+                            {displayskill.section.map(section=>
+                              <React.Fragment key={section.id}>
                                 <Stack direction="row" justifyContent="normal" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
-                                  <Chip label={`Experience`} variant="outlined" sx={{color:"CaptionText", backgroundColor:"slategray"}}/>
-                                  <Typography component="p">  {t(`${sectionone.Experience}`)} </Typography>
-                                </Stack>
-                                <Stack direction="row" justifyContent="normal" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
-                                  <Chip label={`Organization`} variant="outlined" sx={{color:"CaptionText", backgroundColor:"slategray"}}/>
-                                  {sectionone.Organization.map(org=>
-                                  {Object.entries(org).map(item=>
-                                    <Typography component="p">
-                                      <a href={`${item[1]}`}>{t(`${item[0]}`)}</a>
-                                    </Typography>
-                                  )})}
-                                </Stack>
-                                <Stack direction="row" justifyContent="normal" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
-                                  <Chip label={`Position`} variant="outlined" sx={{color:"CaptionText", backgroundColor:"slategray"}}/>
-                                  <Typography component="p">  {t(`${sectionone.Position}`)} </Typography>
+                                  <Chip label={`Projects`} variant="outlined" sx={{color:"white", backgroundColor:"#2e186a"}}/>
+                                  {section.Project.map(project=>
+                                    <Typography component="p" sx={{fontSize:"13px"}}><a href={`${project[1]}`} style={{color:"black"}}>{t(`${project[0]}`)}</a></Typography>
+                                  )}
                                 </Stack>
                               </React.Fragment>
                               )
                             }
-                          </Stack>
-                          <Stack direction="column" spacing={2}>
-                            {skill.sectionTwo.map(sectiontwo=>{
-                              <React.Fragment key={sectiontwo.id}>
-                                <Stack direction="row" justifyContent="normal" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
-                                  <Chip label={`Organization`} variant="outlined" sx={{color:"CaptionText", backgroundColor:"slategray"}}/>
-                                  {sectiontwo.Project.map(org=>
-                                  {Object.entries(org).map(item=>
-                                    <Typography component="p">
-                                      <a href={`${item[1]}`}>{t(`${item[0]}`)}</a>
-                                    </Typography>
-                                  )})}
-                                </Stack>
-                                <Stack direction="row" justifyContent="normal" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
-                                  <Chip label={`Experience`} variant="outlined" sx={{color:"CaptionText", backgroundColor:"slategray"}}/>
-                                  <Typography component="p">  {t(`${sectiontwo.Goal}`)} </Typography>
-                                </Stack>
-                                <Stack direction="row" justifyContent="normal" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
-                                  <Chip label={`Position`} variant="outlined" sx={{color:"CaptionText", backgroundColor:"slategray"}}/>
-                                  <Typography component="p">  {t(`${sectiontwo.JobType}`)} </Typography>
-                                </Stack>
-                              </React.Fragment>
-                            })}
-                          </Stack>
-                        </Stack>
+                          {/* </Col> */}
+                        {/* </Row> */}
                       </PaperContainer>
                     </DivContainer>
                   </Col>
@@ -86,6 +66,12 @@ const Experience = ({ t }: { t: TFunction }) => {
               }
             </Row>
           </Slide>
+        </Stack>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{marginBottom:"10px"}}>
+                {/* {clicked && <LoadingButton type="button" variant="outlined" loading={clicked} loadingPosition="center"
+                sx={{backgroundImage: "linear-gradient(163deg, #a00000fa 0%, #d10050 100%) !important"}}/>}
+                {(skill.length != displayedSkill.length && !clicked) && <Button type="button" disabled={clicked} variant="contained" onClick={loadMore}>Load More</Button>} */}
+          <Button type="button" variant="contained" onClick={loadMore} disabled={clicked} fullWidth sx={{backgroundColor:"#2e186a"}}>Load More</Button>
         </Stack>
       </Container>
     </React.Fragment>
